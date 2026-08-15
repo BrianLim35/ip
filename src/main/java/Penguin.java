dd3f0a09d85ffc58987c235fb7ddcd47a333fa6c
BrianLim35 <brianlim35@gmail.com>
2026-08-15 13:51:22 +0800

    Add user greeting and exit

--- src/main/java/Penguin.java
+++ src/main/java/Penguin.java
@@ -1,10 +1,25 @@
 public class Penguin {
+    private static final String LINE = "----------------------------------------------------------";
+    private static final String BANNER = " ____  _____ _   _  ____ _   _ ___ _   _ \n"
+            + "|  _ \\| ____| \\ | |/ ___| | | |_ _| \\ | |\n"
+            + "| |_) |  _| |  \\| | |  _| | | || ||  \\| |\n"
+            + "|  __/| |___| |\\  | |_| | |_| || || |\\  |\n"
+            + "|_|   |_____|_| \\_|\\____|\\___/|___|_| \\_|";
+    private static final String GREETING_MESSAGE = "Hello! I'm Penguin.\nWhat can I do for you?";
+    private static final String GOODBYE_MESSAGE = "Bye. Hope to see you again soon!";
+
     public static void main(String[] args) {
-        String banner = " ____  _____ _   _  ____ _   _ ___ _   _ \n"
-                + "|  _ \\| ____| \\ | |/ ___| | | |_ _| \\ | |\n"
-                + "| |_) |  _| |  \\| | |  _| | | || ||  \\| |\n"
-                + "|  __/| |___| |\\  | |_| | |_| || || |\\  |\n"
-                + "|_|   |_____|_| \\_|\\____|\\___/|___|_| \\_|";
-        System.out.println(banner);
+        // Chatbot banner
+        System.out.println(LINE);
+        System.out.println(BANNER);
+        System.out.println(LINE);
+
+        // Chatbot greetings
+        System.out.println(GREETING_MESSAGE);
+        System.out.println(LINE);
+
+        // Chatbot exits
+        System.out.println(GOODBYE_MESSAGE);
+        System.out.println(LINE);
     }
 }
